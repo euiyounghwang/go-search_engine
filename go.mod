a07f46a0f73e8576984c6e08c9be960367b2c158
euiyounghwang <marieuig@gmail.com>
2023-12-25 18:50:35 -0600

    Update

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/labstack/echo/v4 v4.11.4
 
 require (
 	github.com/elastic/elastic-transport-go/v8 v8.3.0 // indirect
+	github.com/elastic/go-elasticsearch v0.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/olivere/elastic v6.2.37+incompatible // indirect
@@ -14,6 +15,7 @@ require (
 )
 
 require (
+	github.com/dustin/go-humanize v1.0.1
 	github.com/elastic/go-elasticsearch/v7 v7.17.10
 	github.com/elastic/go-elasticsearch/v8 v8.11.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
--- tools/elasticsearch_api.go
+++ tools/elasticsearch_api.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"time"
 
-	elasticsearch "github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esutil"
-	// elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	// elasticsearch "github.com/elastic/go-elasticsearch/v7"
+
+	"github.com/dustin/go-humanize"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
 )
 
 var (
@@ -33,6 +44,37 @@ type ElasticDocs struct {
 	SomeInt int
 	SomeBool bool
 	Timestamp int64
+}
+
+type Article struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	Published time.Time `json:"published"`
+	Author    Author    `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+var (
+	indexName  string
+	numWorkers int
+	flushBytes int
+	numItems   int
+)
+
+func init() {
+	log.Println("init..")
+	// flag.StringVar(&indexName, "index", "test-bulk-example", "Index name")
+	flag.IntVar(&numWorkers, "workers", runtime.NumCPU(), "Number of indexer workers")
+	flag.IntVar(&flushBytes, "flush", 5e+6, "Flush threshold in bytes")
+	flag.IntVar(&numItems, "count", 10000, "Number of documents to generate")
+	flag.Parse()
+	
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
@@ -125,4 +167,128 @@ func main() {
 		fmt.Println("resMap:", resMap)
 		fmt.Println("\n")
 	}
+	
+	// Generate the articles collection
+	
+	var (
+		articles        []*Article
+		countSuccessful uint64
+
+		// res *esapi.Response
+		// err error
+	)
+
+	names := []string{"Alice", "John", "Mary"}
+	for i := 1; i <= numItems; i++ {
+		articles = append(articles, &Article{
+			ID:        i,
+			Title:     strings.Join([]string{"Title", strconv.Itoa(i)}, " "),
+			Body:      "Lorem ipsum dolor sit amet...",
+			Published: time.Now().Round(time.Second).UTC().AddDate(0, 0, i),
+			Author: Author{
+				FirstName: names[rand.Intn(len(names))],
+				LastName:  "Smith",
+			},
+		})
+	}
+	log.Printf("→ Generated %s articles", humanize.Comma(int64(len(articles))))
+	
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	//
+	// Create the BulkIndexer
+	//
+	// NOTE: For optimal performance, consider using a third-party JSON decoding package.
+	//       See an example in the "benchmarks" folder.
+	//
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:         index,        // The default index name
+		Client:        es_client,               // The Elasticsearch client
+		NumWorkers:    numWorkers,       // The number of worker goroutines
+		FlushBytes:    int(flushBytes),  // The flush threshold in bytes
+		FlushInterval: 30 * time.Second, // The periodic flush interval
+	})
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
+	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	
+	start := time.Now().UTC()
+	
+	// Loop over the collection
+	//
+	for _, a := range articles {
+		// Prepare the data payload: encode article to JSON
+		//
+		data, err := json.Marshal(a)
+		// log.Println((data))
+		if err != nil {
+			log.Fatalf("Cannot encode article %d: %s", a.ID, err)
+		}
+
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		//
+		// Add an item to the BulkIndexer
+		//
+		err = bi.Add(
+			context.Background(),
+			esutil.BulkIndexerItem{
+				// Action field configures the operation to perform (index, create, delete, update)
+				Action: "index",
+
+				// DocumentID is the (optional) document ID
+				DocumentID: strconv.Itoa(a.ID),
+
+				// Body is an `io.Reader` with the payload
+				Body: bytes.NewReader(data),
+
+				// OnSuccess is called for each successful operation
+				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+					atomic.AddUint64(&countSuccessful, 1)
+				},
+
+				// OnFailure is called for each failed operation
+				OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+					if err != nil {
+						log.Printf("ERROR: %s", err)
+					} else {
+						log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+					}
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Unexpected error: %s", err)
+		}
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		if err := bi.Close(context.Background()); err != nil {
+			log.Fatalf("Unexpected error: %s", err)
+		}
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	
+		biStats := bi.Stats()
+	
+		// Report the results: number of indexed docs, number of errors, duration, indexing rate
+		//
+		log.Println(strings.Repeat("▔", 65))
+	
+		dur := time.Since(start)
+	
+		if biStats.NumFailed > 0 {
+			log.Fatalf(
+				"Indexed [%s] documents with [%s] errors in %s (%s docs/sec)",
+				humanize.Comma(int64(biStats.NumFlushed)),
+				humanize.Comma(int64(biStats.NumFailed)),
+				dur.Truncate(time.Millisecond),
+				humanize.Comma(int64(1000.0/float64(dur/time.Millisecond)*float64(biStats.NumFlushed))),
+			)
+		} else {
+			log.Printf(
+				"Sucessfuly indexed [%s] documents in %s (%s docs/sec)",
+				humanize.Comma(int64(biStats.NumFlushed)),
+				dur.Truncate(time.Millisecond),
+				humanize.Comma(int64(1000.0/float64(dur/time.Millisecond)*float64(biStats.NumFlushed))),
+			)
+		}
+	}
+
 }
