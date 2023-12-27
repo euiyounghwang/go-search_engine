e4ea2993808168766db110f91bd89e7b7fb53535
euiyounghwang <marieuig@gmail.com>
2023-12-27 17:48:00 -0600

    Update

--- go_convey.sh
+++ go_convey.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # cd /Users/euiyoung.hwang/go/pkg/mod/github.com/smartystreets/goconvey@v1.8.1
 # cd /Users/euiyoung.hwang/go/bin
-/Users/euiyoung.hwang/go/bin/goconvey --workDir=$SCRIPTDIR/tests
+/Users/euiyoung.hwang/go/bin/goconvey --workDir=$SCRIPTDIR/tests --port=7090
