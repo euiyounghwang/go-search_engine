5268c4ca4d9deeba7cd6d9a275a9caa3a82839b9
euiyounghwang <marieuig@gmail.com>
2023-12-26 23:46:30 -0600

    Update README.md

--- go_convey.sh
+++ go_convey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # cd /Users/euiyoung.hwang/go/pkg/mod/github.com/smartystreets/goconvey@v1.8.1
-cd /Users/euiyoung.hwang/go/bin
-./goconvey
+# cd /Users/euiyoung.hwang/go/bin
+/Users/euiyoung.hwang/go/bin/goconvey
