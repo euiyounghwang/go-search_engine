b3ec2309909e54abbb3e5bc68b74999d134c88b5
euiyounghwang <marieuig@gmail.com>
2023-12-27 21:28:47 -0600

    Update

--- tools/bulk_index_script.go
+++ tools/bulk_index_script.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"log"
 
-	my_elasticsearch "go-search_engine/lib"
+	my_elasticsearch "go-search_engine/lib/elasticsearch"
 	// my_elasticsearch "github.com/euiyounghwang/go-search_engine/tools/lib"
 )
 
